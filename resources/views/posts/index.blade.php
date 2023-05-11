d568c93412a1d5b662308fb3e7234384d649c744
bagasjs <nakanoyoichi83@gmail.com>
2023-05-11 19:13:35 +0700

    Modify Dockerfile

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,10 +13,10 @@
     <section class="bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
             <h3 class="text-white font-bold text-xl border-b pb-2 border-white">Latest Post</h3>
-            <h1 class="my-2 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">{{ $posts[0]->title }}</h1>
-            <p class="mb-8 text-lg font-normal lg:text-xl sm:px-16 lg:px-48 text-gray-400">{{ $posts[0]->excerpt }}</p>
+            <h1 class="my-2 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">{{ $posts->first()->title }}</h1>
+            <p class="mb-8 text-lg font-normal lg:text-xl sm:px-16 lg:px-48 text-gray-400">{{ $posts->first()->excerpt }}</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <a href="/posts/{{ $posts[0]->slug }}" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-900">
+                <a href="/posts/{{ $posts->first()->slug }}" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-900">
                     Read this post
                     <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
